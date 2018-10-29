cb0207cebdb6737f8c2dcd71a654231f87b34dab
John Thompson <john@springframework.guru>
2018-10-29 19:36:47 -0400

    refactor package

---
+++ src/test/java/guru/springframework/JUnitExtensionTest.java
@@ -0,0 +1,23 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Map;
+
+/**
+ * Created by jt on 2018-10-29.
+ */
+@ExtendWith(MockitoExtension.class)
+public class JUnitExtensionTest {
+
+    @Mock
+    Map<String, Object> mapMock;
+
+    @Test
+    void testMock() {
+        mapMock.put("keyvalue", "foo");
+    }
+}
