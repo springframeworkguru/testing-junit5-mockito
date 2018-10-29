ccd9c32efbb6b2b2eb17b3fc900fb77021b3d964
John Thompson <john@springframework.guru>
2018-10-29 19:46:38 -0400

    adding inject mocks example

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -0,0 +1,29 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.model.Speciality;
+import guru.springframework.sfgpetclinic.repositories.SpecialtyRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class SpecialitySDJpaServiceTest {
+
+    @Mock
+    SpecialtyRepository specialtyRepository;
+
+    @InjectMocks
+    SpecialitySDJpaService service;
+
+    @Test
+    void deleteById() {
+        service.deleteById(1l);
+    }
+
+    @Test
+    void testDelete() {
+        service.delete(new Speciality());
+    }
+}
