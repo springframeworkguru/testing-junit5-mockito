b96723c2eb74fdc561a6900eb329f4966176c35b
John Thompson <john@springframework.guru>
2018-10-29 20:03:58 -0400

    adding verify examples

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,8 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.mockito.Mockito.*;
+
 @ExtendWith(MockitoExtension.class)
 class SpecialitySDJpaServiceTest {
 
@@ -20,6 +22,35 @@ class SpecialitySDJpaServiceTest {
     @Test
     void deleteById() {
         service.deleteById(1l);
+        service.deleteById(1l);
+
+        verify(specialtyRepository, times(2)).deleteById(1l);
+    }
+
+    @Test
+    void deleteByIdAtLeast() {
+        service.deleteById(1l);
+        service.deleteById(1l);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+    }
+
+    @Test
+    void deleteByIdAtMost() {
+        service.deleteById(1l);
+        service.deleteById(1l);
+
+        verify(specialtyRepository, atMost(5)).deleteById(1l);
+    }
+
+    @Test
+    void deleteByIdNever() {
+        service.deleteById(1l);
+        service.deleteById(1l);
+
+        verify(specialtyRepository, atLeastOnce()).deleteById(1l);
+
+        verify(specialtyRepository, never()).deleteById(5L);
     }
 
     @Test
