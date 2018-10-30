d3fe5d1e8e215aee7e5f030dd1a5844ed4c75aee
John Thompson <john@springframework.guru>
2018-10-30 19:22:36 -0400

    completing assignment

---
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/VetSDJpaServiceTest.java
@@ -0,0 +1,27 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import guru.springframework.sfgpetclinic.repositories.VetRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.mockito.Mockito.verify;
+
+@ExtendWith(MockitoExtension.class)
+class VetSDJpaServiceTest {
+
+    @Mock
+    VetRepository vetRepository;
+
+    @InjectMocks
+    VetSDJpaService service;
+
+    @Test
+    void deleteById() {
+        service.deleteById(1L);
+
+        verify(vetRepository).deleteById(1L);
+    }
+}
